634cd25ceaf377c08bbd77d920fb2a8b37e902de
blsdxvmip <pichez232@gmail.com>
2026-04-05 18:39:28 +0300

    Сделаны страницы авторизации и регистрации

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def register():
+    return render_template('register.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
